355e8d900ea791a5424a2a2bf2b18bb56817068e
Grace <carlson23213@lcsd2kids.org>
2021-04-29 11:17:57 -0600

    it worked after i changed () to (p)

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 const gridContainer = document.querySelector("#grid-container");
+const gridCell = document.createElement("p")
+gridCell.textContent = "did it work";
+gridContainer.appendChild(gridCell);
