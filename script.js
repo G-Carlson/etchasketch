ffd12842f6698ca399521fea8d8d40713bf23433
Grace <carlson23213@lcsd2kids.org>
2021-05-05 10:20:07 -0600

    did the code one thing wrong added color tho

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const gridContainer = document.querySelector("#grid-container");
+
+let numOfTimes = 16**3;
+for(let i = 0; i<numOfTimes; i+++){
+    const singlecell = document.createElement ("div");
+    singlecell.textContent = "hello";
+    singlecell.style.color = "white";
+    singlecell.classList.add("bg-red");
+    singlecell.addEventListener('mouseover',()=>) { 
+        singlecell.textcontent = "blub";
+        singlecell.classList.toggle("bg-pink");
+    });
+    gridContainer.appendChild(singlecell);
+}
