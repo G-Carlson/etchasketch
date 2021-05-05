cc2e1c820ca51de47254eb18b6c8e7cf79bb5469
Grace <carlson23213@lcsd2kids.org>
2021-05-05 10:05:39 -0600

    foud the paper to help have the right code

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
 const gridContainer = document.querySelector("#grid-container");
-const gridCell = document.createElement("p")
-gridCell.textContent = "did it work";
-gridContainer.appendChild(gridCell);
