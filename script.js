5dd6e0b7350a617645e48c68e7b8c058f0c98357
Grace <carlson23213@lcsd2kids.org>
2021-05-05 11:12:29 -0600

    fixed it and now its working had css wrong

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ for(let i = 0; i<numOfTimes; i++){
     singlecell.textContent = "hello";
     singlecell.style.color = "white";
     singlecell.classList.add("bg-red");
-    singlecell.addEventListener('click',() => { 
-       alert("hello World");
+    singlecell.addEventListener('mouseover',() => { 
+        singlecell.classList.add("bg-cyan");
     });
     gridContainer.appendChild(singlecell);
 }
