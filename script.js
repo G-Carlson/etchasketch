b1ff2931d8e3112cc75d4cfcbc2c74eec14a7e40
Grace <carlson23213@lcsd2kids.org>
2021-05-07 10:51:48 -0600

    i dnt know what im doing

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let numOfTimes = 256;
 for(let i = 0; i<numOfTimes; i++){
     const singlecell = document.createElement ("div");
     singlecell.textContent = ".";
-    singlecell.style.color = "rgb(250, 2, 138) ";
+    singlecell.style.color = "black";
     singlecell.classList.add("bg-red");
-    singlecell.addEventListener('click',() => { 
+    singlecell.addEventListener('mouseover',() => { 
         singlecell.classList.toggle("bg-cyan");
-        singlecell.style.color = "hsl(249, 100%, 47%) ";
+        //singlecell.style.color = "hsl(249, 100%, 47%) ";
     });
     gridContainer.appendChild(singlecell);
 }
